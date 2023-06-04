fd6fa450720956c6cf25a181a7d2c5d49ab3eac1
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-06-04 13:36:59 +0200

    build: added version script

---
+++ scripts/postversion.js
@@ -0,0 +1,27 @@
+const pkg = require("../package.json");
+const fs = require("fs-extra");
+const path = require("path");
+
+const emptyProjectPkgPath = path.join(__dirname, "..", "files", "empty-project", "package.json");
+
+(async () => {
+    await fs.readFile(emptyProjectPkgPath, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+
+            return;
+        }
+
+        const obj = JSON.parse(data);
+
+        obj["dependencies"]["@tsparticles/cli"] = pkg.version;
+
+        const result = JSON.stringify(obj, undefined, 2);
+
+        fs.writeFile(emptyProjectPkgPath, `${result}\n`, "utf8", (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+})();
