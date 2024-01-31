90234ac642e32ac5c51a0d5ea684ace45ff37bfe
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2024-01-31 09:17:57 +0100

    build: updated deps and template generation

--- files/create-preset/src/index.ts
+++ files/create-preset/src/index.ts
@@ -1,11 +1,12 @@
 import type { Engine } from "@tsparticles/engine";
-import { options } from "./options";
 
 /**
  *
  * @param engine - the engine instance to load the preset into
  */
 export async function loadTemplatePreset(engine: Engine): Promise<void> {
+    const { options } = await import("./options");
+
     // TODO: additional modules must be loaded here
 
     // Adds the preset to the engine, with the given options
--- files/create-shape/src/ShapeDrawer.ts
+++ files/create-shape/src/ShapeDrawer.ts
@@ -1,7 +1,7 @@
 import type { IShapeDrawData, IShapeDrawer } from "@tsparticles/engine";
 
 export class ShapeDrawer implements IShapeDrawer {
-    draw(_data: IShapeDrawData): void {
+    async draw(_data: IShapeDrawData): Promise<void> {
         // draw the particle using the context
         // which is already centered in the particle position
         // colors are already handled, just draw the shape
@@ -10,5 +10,8 @@ export class ShapeDrawer implements IShapeDrawer {
         // pixelRatio is the canvas ratio used by the tsParticles instance, you may need it for density-independent shapes
         // the parameters have an underscore prefix because they're not used in this example
         // the underscore prefix can be removed for used parameters, the unused ones can be removed too
+
+        // remove this if there's already an await call
+        await Promise.resolve();
     }
 }
