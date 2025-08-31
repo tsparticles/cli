366cb9189c857f67a2e50269695091f68011b674
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2025-08-31 23:08:34 +0200

    build: updated deps

--- files/empty-project/webpack.config.js
+++ files/empty-project/webpack.config.js
@@ -1,11 +1,4 @@
 import {loadParticlesTemplate} from "@tsparticles/webpack-plugin";
-import {fileURLToPath} from 'url';
-import path from 'path';
-import fs from 'fs';
-
-const __filename = fileURLToPath(import.meta.url),
-    __dirname = path.dirname(__filename),
-    pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')),
-    version = pkg.version;
+import { version } from "./package.json" assert { type: "json" };
 
 export default loadParticlesTemplate({moduleName: "empty", templateName: "Empty", version, dir: __dirname});
