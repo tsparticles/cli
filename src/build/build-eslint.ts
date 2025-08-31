c23627f5d14adfed804e8ef86a6b67beb9a04150
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2025-08-31 18:50:31 +0200

    build: updated deps and fixed eslint build

--- src/build/build-eslint.ts
+++ src/build/build-eslint.ts
@@ -1,4 +1,6 @@
 import { ESLint } from "eslint";
+import { defineConfig } from "eslint/config";
+import tsParticlesESLintConfig from "@tsparticles/eslint-config";
 
 /**
  * @param ci -
@@ -10,7 +12,7 @@ export async function lint(ci: boolean): Promise<boolean> {
     let res: boolean;
 
     try {
-        const eslint = new ESLint({ fix: !ci });
+        const eslint = new ESLint({ baseConfig: defineConfig([tsParticlesESLintConfig]), fix: !ci });
 
         const results = await eslint.lintFiles(["src"]),
             errors = ESLint.getErrorResults(results);
