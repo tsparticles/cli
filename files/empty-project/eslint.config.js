278e13db2e5a3875a7fc128a522c981c83005f75
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2025-12-20 19:32:37 +0100

    build: updated cli files for create

--- files/empty-project/eslint.config.js
+++ files/empty-project/eslint.config.js
@@ -1,6 +1,18 @@
-import tsParticlesESLintConfig from "@tsparticles/eslint-config";
+import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "eslint/config";
+import tsParticlesESLintConfig from "@tsparticles/eslint-config";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
     tsParticlesESLintConfig,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: "./tsconfig.json",
+                tsconfigRootDir: __dirname,
+            },
+        },
+    },
 ]);
