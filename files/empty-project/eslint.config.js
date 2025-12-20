a346f80c0ee10621bfcc41e992ba44694882dcdf
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2025-12-20 19:38:40 +0100

    build: fix didn't work, reverted

--- files/empty-project/eslint.config.js
+++ files/empty-project/eslint.config.js
@@ -1,18 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import { defineConfig } from "eslint/config";
 import tsParticlesESLintConfig from "@tsparticles/eslint-config";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig([
     tsParticlesESLintConfig,
-    {
-        languageOptions: {
-            parserOptions: {
-                project: "./tsconfig.json",
-                tsconfigRootDir: __dirname,
-            },
-        },
-    },
 ]);
